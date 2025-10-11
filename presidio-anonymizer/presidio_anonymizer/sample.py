0c498537c59c019146d8fc8bbffef212b2237a01
Fahad Akon <75163075+md-fahad-akon@users.noreply.github.com>
2025-10-11 05:06:28 +0000

    make sample.py testable

--- presidio-anonymizer/presidio_anonymizer/sample.py
+++ presidio-anonymizer/presidio_anonymizer/sample.py
@@ -1,7 +1,7 @@
 from presidio_anonymizer import AnonymizerEngine
 from presidio_anonymizer.entities import RecognizerResult, OperatorConfig
 
-def sample_run_anonymizer():
+def sample_run_anonymizer(text_to_anonymize: str, start_index: int, end_index: int):
     # Initialize the engine
     engine = AnonymizerEngine()
 
@@ -9,12 +9,19 @@ def sample_run_anonymizer():
     # analyzer results (potentially coming from presidio-analyzer) and
     # Operators to get the anonymization output:
     result = engine.anonymize(
-        text=input("text: "),
-        analyzer_results=[RecognizerResult(entity_type="PERSON", start=int(input("start: ")), end=int(input("end: ")), score=0.8)],
-        operators={"PERSON": OperatorConfig("replace", {"new_value": "BIP"})}
+        text=text_to_anonymize,
+        analyzer_results=[
+            RecognizerResult(
+                entity_type="PERSON",
+                start=start_index,
+                end=end_index,
+                score=0.8,
+            )
+        ],
+        operators={"PERSON": OperatorConfig("replace", {"new_value": "BIP"})},
     )
 
-    print(result)
+    return result
 
     # input should be:
     # text: My name is Bond.
@@ -29,4 +36,10 @@ def sample_run_anonymizer():
     # ]
 
 if __name__ == "__main__": 
-    sample_run_anonymizer();
+    text = "My name is Bond."
+    start = 11
+    end = 15
+
+    anonymized_result = sample_run_anonymizer(text, start, end)
+    
+    print(anonymized_result)
